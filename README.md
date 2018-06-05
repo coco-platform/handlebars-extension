086cc5416d55587c70f71b705b4bd173d22c092b
yang.yuncai <383235388@qq.com>
2018-06-05 15:58:20 +0800

    feature: implement link, inline expressions.

--- README.md
+++ README.md
@@ -5,12 +5,51 @@
 ![Package Dependency](https://david-dm.org/coco-template/@coco-platform/handlebars-extension.svg?style=flat)
 ![Package DevDependency](https://david-dm.org/coco-template/@coco-platform/handlebars-extension/dev-status.svg?style=flat)
 
-A universal seed for nodejs usage..
+A specific handlebars extension collection within MVC.
 
 ## Usage
 
-```shell
-npm run dev;
+```javascript
+const { inline, link } = require('@coco-platform/handlebars-extension');
+const context = {
+  resources: [
+    'https://static.zhihu.com/heifetz/main.app.c994694b7b8c848b345c.css',
+    {
+      rel: 'dns-prefetch',
+      href: `//static.zhimg.com`,
+    },
+    'https://static.zhihu.com/heifetz/main.app.c994694b7b8c848b345c.js',
+    {
+      src:
+        'https://static.zhihu.com/heifetz/main.signflow.6af7025179e6b1979aca.js',
+      crossorigin: true,
+    },
+  ],
+  criticals: [
+    {
+      type: 'stylesheet',
+      content:
+        '.CloseIcon-icon-2xww{transition:opacity .3s ease-out}.CloseIcon-icon-2xww:hover{opacity:.8}',
+    },
+    {
+      type: 'script',
+      content: `(function($) {$(document).ready(() => {});})(jQuery);`,
+    },
+  ],
+};
+
+hbs.registerHelper('inline', inline);
+hbs.registerHelper('link', link);
+```
+
+```handlebars
+{{#each criticals as | critical |}}
+  {{inline critical}}
+{{/each}}
+
+{{#each resources as | resource |}}
+  {{link resource}}
+{{/each}}
 ```
 
 # Licence
--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,12 +1,75 @@
 /**
- * @description - suits example
+ * @description - handlebars extensions
  * @author - huang.jian <hjj491229492@hotmail.com>
  */
 
-const sum = require('../lib');
+// package
+const path = require('path');
+const fs = require('fs');
+const hbs = require('handlebars');
+// internal
+const { inline, link } = require('../');
+// scope
+const options = { encoding: 'utf8' };
 
-describe('simple suits', () => {
-  it('simple case', () => {
-    expect(sum(1, 2, 3)).toEqual(6);
+describe('@coco-platform/handlebars-extension', () => {
+  beforeAll(() => {
+    hbs.registerHelper('inline', inline);
+    hbs.registerHelper('link', link);
+  });
+
+  it('should support inline expression', () => {
+    const demo = path.resolve(__dirname, '__fixture__', 'inline.hbs');
+    const template = fs.readFileSync(demo, options);
+    const context = {
+      criticals: [
+        {
+          type: 'stylesheet',
+          content:
+            '.CloseIcon-icon-2xww{transition:opacity .3s ease-out}.CloseIcon-icon-2xww:hover{opacity:.8}',
+        },
+        {
+          type: 'script',
+          content: `(function($) {$(document).ready(() => {});})(jQuery);`,
+        },
+        {},
+      ],
+    };
+    const result = hbs.compile(template)(context);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should support link expression', () => {
+    const demo = path.resolve(__dirname, '__fixture__', 'link.hbs');
+    const template = fs.readFileSync(demo, options);
+    const context = {
+      resources: [
+        'https://static.zhihu.com/heifetz/main.app.c994694b7b8c848b345c.css',
+        {
+          href:
+            'https://static.zhihu.com/heifetz/main.app.c994694b7b8c848b345c.css',
+        },
+        {
+          rel: 'dns-prefetch',
+          href: `//static.zhimg.com`,
+        },
+        'https://static.zhihu.com/heifetz/main.app.c994694b7b8c848b345c.js',
+        {
+          src:
+            'https://static.zhihu.com/heifetz/main.signflow.6af7025179e6b1979aca.js',
+          crossorigin: true,
+        },
+        {},
+      ],
+    };
+    const result = hbs.compile(template)(context);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should return empty with un-supported resource', () => {
+    expect(inline(200)).toEqual('');
+    expect(link(200)).toEqual('');
   });
 });
